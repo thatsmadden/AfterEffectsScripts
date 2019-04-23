3b0dfede360ae7eff81bd524164b28db5caf249d
thatsmadden <thatsmadden@users.noreply.github.com>
2019-04-22 21:14:47 -0400

    Update sm_setIntervalMarkers.jsx

--- sm_setIntervalMarkers.jsx
+++ sm_setIntervalMarkers.jsx
@@ -4,9 +4,11 @@ var comp = app.project.activeItem;
 var spacing = parseInt(prompt("How many frames between markers?", "144"));
 var numMarkers = Math.floor(((comp.duration-comp.time)*comp.frameRate)/spacing);
 var mark = new MarkerValue("");
-app.beginUndoGroup("sm_setIntervalMarkers");
-for (var i = 0; i <= numMarkers; i++){
 
-  comp.markerProperty.setValueAtTime((i*spacing)/comp.frameRate + comp.time, mark);
-}
-app.endUndoGroup();
+if (spacing != null && spacing !=0){
+  app.beginUndoGroup("sm_setIntervalMarkers");
+  for (var i = 0; i <= numMarkers; i++){
+    comp.markerProperty.setValueAtTime((i*spacing)/comp.frameRate + comp.time, mark);
+  }
+  app.endUndoGroup();
+};
