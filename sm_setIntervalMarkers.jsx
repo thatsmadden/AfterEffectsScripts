480268934a4dcb3d18ca5e3a5b4d746afc2b1612
thatsmadden <thatsmadden@users.noreply.github.com>
2019-11-05 14:32:27 -0500

    Update sm_setIntervalMarkers.jsx

--- sm_setIntervalMarkers.jsx
+++ sm_setIntervalMarkers.jsx
@@ -1,4 +1,13 @@
-//sm_setIntervalMarkers 2019
+//sm_setIntervalMarkers v1.0
+// Copyright (c) 2019 Steve Madden. All rights reserved.
+
+// Adds markers to the current composition spaced out the number of frames provided by the user, starting at the playhead.
+// To use: Make sure your composition timeline is focused in the interface, your playhead is where you want your first marker, and run the script, and follow the prompts.
+
+// Legal stuff:
+// This script is provided "as is," without warranty of any kind, expressed or
+// implied. In no event shall the author be held liable for any damages
+// arising in any way from the use of this script.
 
 var comp = app.project.activeItem;
 var spacing = parseInt(prompt("How many frames between markers?", "144"));
